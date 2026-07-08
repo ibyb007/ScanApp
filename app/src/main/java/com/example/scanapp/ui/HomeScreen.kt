3a26eab785d17b4f2d15a8ae5c311b8479288d4f
ibyb007 <58368196+ibyb007@users.noreply.github.com>
2026-07-08 09:59:22 +0530

    Bug fixes.

--- app/src/main/java/com/example/scanapp/ui/HomeScreen.kt
+++ app/src/main/java/com/example/scanapp/ui/HomeScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.scrollBy
 import kotlinx.coroutines.delay
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
